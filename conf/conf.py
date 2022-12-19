f53692a8b15362974aa987618c4d585f1da62fc1
alinasubbotina <50928076+alinasubbotina@users.noreply.github.com>
2022-12-19 23:07:27 +0300

    Create conf.py

---
+++ conf/conf.py
@@ -0,0 +1,8 @@
+import logging
+from dynaconf import Dynaconf
+
+
+# specifying logging level
+logging.basicConfig(level=logging.INFO)
+
+settings = Dynaconf(settings_file='conf/setting.toml', environments=False)
