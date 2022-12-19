94ad0da3687688a686250c2cfc948230f6f67148
alinasubbotina <50928076+alinasubbotina@users.noreply.github.com>
2022-12-19 23:12:30 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,22 +1,16 @@
-from model.ml_models import split, train_random_forest, train_naive_bayes, logging, predict
+from model.ml_models import prediction
 from conf.conf import settings
-from connector.pg_connector import get_data
+import argparse
 
-df = get_data(settings.data_set)
-X_train, X_test, y_train, y_test = split(df)
 
-# Random Forest
-clf = train_random_forest(X_train, y_train, settings.rf_conf)
+parser = argparse.ArgumentParser(description="Taking parameters for models")
+parser.add_argument(
+    '--prediction_model_path',
+    type=str,
+    default=settings.rf_conf,
+    help=f'Provide a path to the model (default: {settings.rf_conf})'
+)
+model_var = parser.parse_args()
+path = model_var.prediction_model_path
 
-logging.info(f'Accuracy for Random Forest model is {clf.score(X_test, y_test)}')
-
-response = predict(X_test, settings.rf_conf)
-logging.info(f'Prediction for Random Forest model is {clf.predict(X_test)}')
-
-# Naive Bayes
-clf = train_naive_bayes(X_train, y_train, settings.lr_conf)
-
-logging.info(f'Accuracy for Naive Bayes model is {clf.score(X_test, y_test)}')
-
-response = predict(X_test, settings.lr_conf)
-logging.info(f'Prediction for Naive Bayes model is {clf.predict(X_test)}')
+prediction(settings.data_set, path)
